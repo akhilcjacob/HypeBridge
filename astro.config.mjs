d98c12bec4fa7b566302a26b5631dab2ff7295f5
Akhil Jacob <akhilcjacob@gmail.com>
2024-10-12 16:29:00 +0100

    Adding github page deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://theHypebridge.com',
   integrations: [react(), tailwind()]
 });
